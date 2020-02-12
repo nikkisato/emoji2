2967e2d0cebe469a68e046348180f0c1ff1530e4
Nikki <nikki.satopdx@gmail.com>
2020-02-12 15:07:37 -0800

    inital commit

---
+++ src/components/actions/moodActions.js
@@ -0,0 +1,19 @@
+export const DRINK_COFFEE = 'DRINK_COFFEE';
+export const drinkCoffee = () => ({
+  type: DRINK_COFFEE
+});
+
+export const EAT_SNACK = 'EAT_SNACK';
+export const eatSnacks = () => ({
+  type: EAT_SNACK
+});
+
+export const TAKE_NAP = 'TAKE_NAP';
+export const takeNaps = () => ({
+  type: TAKE_NAP
+});
+
+export const STUDY = 'STUDY';
+export const study = () => ({
+  type: STUDY
+});
