dfd042b96c85e529c66027362d26984a83667443
Nikki <nikki.satopdx@gmail.com>
2020-02-13 16:12:18 -0800

    finishing up tests

--- src/selectors/moodSelector.test.js
+++ src/selectors/moodSelector.test.js
@@ -1,93 +1,79 @@
-import { getFace } from './moodSelectors';
+import Moods, {
+  isTired,
+  isHyper,
+  isEducated,
+  isHungry,
+  getFace
+} from './moodSelectors';
 
-describe('mood selector', () => {
-  it('can return correct mood emoji', () => {
-    const state = {
-      coffees: 0,
-      snacks: 0,
-      naps: 0,
-      studies: 0
-    };
-    const face = getFace(state);
-    expect(face).toEqual('🤬');
-  });
+describe('Moods', () => {
+  describe('mood selectors', () => {
+    it('knows if it is tired', () => {
+      expect(isTired({ coffees: 0, naps: 0 })).toBeTruthy();
+    });
 
-  it('can return Smiley mood emoji', () => {
-    const state = {
-      coffees: 1,
-      snacks: 2,
-      naps: 1,
-      studies: 0
-    };
+    it('knows if it is not tired', () => {
+      expect(isTired({ coffees: 1, naps: 0 })).toBeFalsy();
+    });
 
-    const face = getFace(state);
+    it('knows if it is hyper', () => {
+      expect(isHyper({ coffees: 4 })).toBeTruthy();
+    });
 
-    expect(face).toEqual('😀');
-  });
+    it('knows if it is not hyper', () => {
+      expect(isHyper({ coffees: 3 })).toBeFalsy();
+    });
 
-  it('can return Educated mood emoji', () => {
-    const state = {
-      coffees: 0,
-      snacks: 0,
-      naps: 1,
-      studies: 7
-    };
-    const face = getFace(state);
-    expect(face).toEqual('🤯');
-  });
+    it('knows if it is educated', () => {
+      expect(isEducated({ studies: 3 })).toBeTruthy();
+    });
 
-  it('can return Hyper and hungry mood emoji', () => {
-    const state = {
-      coffees: 4,
-      snacks: 0,
-      naps: 0,
-      studies: 0
-    };
-    const face = getFace(state);
-    expect(face).toEqual('🤮');
-  });
+    it('knows if it is not educated', () => {
+      expect(isEducated({ studies: 2 })).toBeFalsy();
+    });
 
-  it('can return tired mood emoji', () => {
-    const state = {
-      coffees: 0,
-      snacks: 6,
-      naps: 0,
-      studies: 6
-    };
-    const face = getFace(state);
-    expect(face).toEqual('😴');
-  });
+    it('knows if it is hungry', () => {
+      expect(isHungry({ snacks: 0 })).toBeTruthy();
+    });
 
-  it('can return Hyper mood emoji', () => {
-    const state = {
-      coffees: 4,
-      snacks: 0,
-      naps: 0,
-      studies: 4
-    };
-    const face = getFace(state);
-    expect(face).toEqual('🤮');
-  });
+    it('knows if it is not hungry', () => {
+      expect(isHungry({ studies: 2 })).toBeFalsy();
+    });
 
-  it('can return angry emoji', () => {
-    const state = {
-      coffees: 0,
-      snacks: 0,
-      naps: 0,
-      studies: 0
-    };
-    const face = getFace(state);
-    expect(face).toEqual('🤬');
-  });
+    it('is very angry if tired and hungry', () => {
+      expect(getFace({ coffees: 0, naps: 0, snacks: 0 })).toEqual('🤬');
+    });
 
-  it('can return HYPER emoji', () => {
-    const state = {
-      coffees: 2,
-      snacks: 0,
-      naps: 0,
-      studies: 3
-    };
-    const face = getFace(state);
-    expect(face).toEqual('🤯');
+    it('is sick if hyper and hungry', () => {
+      expect(getFace({ coffees: 4, snacks: 0 })).toEqual('🤮');
+    });
+
+    it('is sleeping if tired', () => {
+      expect(getFace({ coffees: 4, snacks: 0 })).toEqual('🤮');
+    });
+
+    it('is crazy if hyper', () => {
+      expect(getFace({ coffees: 4, naps: 0, snacks: 0, studies: 0 })).toEqual(
+        '🤮'
+      );
+    });
+
+    it('is mindblown if educated', () => {
+      expect(getFace({ coffees: 1, naps: 0, snacks: 1, studies: 3 })).toEqual(
+        '🤯'
+      );
+    });
+
+    it('is angry if hungry', () => {
+      expect(getFace({ coffees: 1, naps: 0, snacks: 0, studies: 0 })).toEqual(
+        '😡'
+      );
+    });
+
+    it('is happy if not tired, hungry, educated, or hungry', () => {
+      expect(getFace({ coffees: 1, naps: 0, snacks: 1, studies: 0 })).toEqual(
+        '😀'
+      );
+    });
   });
 });
